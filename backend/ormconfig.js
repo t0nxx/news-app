adc5b7b4a5f0f6733f7b8e1712452d355057480c
t0nxx <mahmoudko1500@hotmail.com>
2019-07-11 21:04:25 +0200

    update db password

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -3,7 +3,7 @@ module.exports = {
   "host": "localhost",
   "port": 3306,
   "username": "root",
-  "password": "",
+  "password": "123456",
   "database": "news_app",
   "entities": [__dirname + '/../**/*.entity{.ts,.js}'],
   "synchronize": true,
