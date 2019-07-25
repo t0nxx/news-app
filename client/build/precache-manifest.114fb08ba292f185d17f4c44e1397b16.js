b4f8b46f4e36bc1421387b0d32ba7d8f8aa63e85
t0nxx <mahmoudko1500@hotmail.com>
2019-07-25 17:06:30 +0200

    Untrack files in .gitignore

---
+++ client/build/precache-manifest.114fb08ba292f185d17f4c44e1397b16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accee1a9baf630d0b34f45e291f685f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd98db51f08d93047d5e",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b392c3d77d2b45ddcdb3",
+    "url": "/static/js/2.d612552a.chunk.js"
+  },
+  {
+    "revision": "cd98db51f08d93047d5e",
+    "url": "/static/js/main.941a1eb3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
