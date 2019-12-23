097cf5ec378312a0897f01194ec44e8f7212c730
t0nxx <mahmoudko1500@hotmail.com>
2019-12-23 19:27:46 +0200

    database charset

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -41,7 +41,7 @@ import { ReportModule } from './report/report.module';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
       logging: true,
-      charset: 'UTF8MB4_GENERAL_CI',
+      charset: 'UTF8_GENERAL_CI',
     }),
   ],
   controllers: [AppController],
